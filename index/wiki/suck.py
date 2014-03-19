ec7edccffe24159223ad0f84f087f3f4ebfb7b2d
twobit <twobit@lavabit.com>
2014-03-19 23:48:51 +0100

    make it better

--- index/wiki/suck.py
+++ index/wiki/suck.py
@@ -1,26 +1,35 @@
+#!/usr/bin/env python
 import xml.etree.ElementTree as ET
 import urllib2
 import string
 
-URL = """http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&rvprop=content&generator=allpages&gaplimit=%d&gapfilterredir=nonredirects&gapfrom=%s"""
 
-prefix = "A"
+
+
+prefix = "B"
 gaplimit = 300
 
 
-for prefix in string.uppercase:
+URL = """http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=revisions&rvprop=content&generator=allpages&gaplimit=%d&gapfilterredir=nonredirects&gapfrom=%s"""
+
+#for prefix in string.uppercase:
+
+while True:
+
 	name = "%s-%d.txt" % (prefix, gaplimit)
 	print name
 
-	url = URL % (gaplimit, prefix)
+	url = URL % (gaplimit + 1, prefix)
 	content = urllib2.urlopen(url).read()
-	print content
 	root = ET.fromstring(content)
 
 
 	f = file(name, "w")
-	for page in root[1][0]:
-	#	print page.attrib["title"]
-		#print page[0][0].text.encode("u8") + '\0'
+
+	for i in range(gaplimit):
+		page = root[1][0][i]
+		#title = page.attrib["title"]
 		f.write(page[0][0].text.encode("u8") + '\0')
+
+	prefix = urllib2.quote(root[1][0][gaplimit].attrib["title"].encode("utf-8"))
 
