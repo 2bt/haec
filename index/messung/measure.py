8da31a7434f9cd355b97fd59ca1c726e063520c0
twobit <twobit@lavabit.com>
2014-03-25 01:17:19 +0100

    more cpu freq

--- index/messung/measure.py
+++ index/messung/measure.py
@@ -2,12 +2,17 @@
 
 import os
 
-SAMPLES = 5
+SAMPLES = 3
+#freqs = [120, 240, 480, 960, 1008]
+freqs = [ 30, 48, 60, 72, 84, 96, 120, 132, 144, 156, 168, 180, 192, 204,
+	216, 240, 264, 288, 336, 360, 384, 408, 480, 528, 600, 648, 672, 696,
+	720, 744, 768, 816, 864, 912, 960, 1008 ]
+
 
 os.system("""
 cpufreq-set --governor userspace
 cpufreq-set --min 30000
-cpufreq-set --max 1010000
+cpufreq-set --max 1008000
 """)
 
 
@@ -27,8 +32,10 @@ for cpus in [1, 2]:
 	for mode in modes:
 		threads = 1 if mode == "mr" else 2
 
-		for freq in [120, 240, 480, 960, 1010]:
+		for freq in freqs:
 			os.system("cpufreq-set --freq %d000" % freq)
+			f = int(file("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq").read()) / 1000
+			assert freq == f, "%d != %d" % (freq, f)
 
 			for input_len in 25, 50, 100:
 				t = 0
@@ -41,6 +48,6 @@ for cpus in [1, 2]:
 
 
 os.system("""
-cpufreq-set --freq 1010000
+cpufreq-set --freq 1008000
 echo 1 > /sys/devices/system/cpu/cpu1/online
 """)
