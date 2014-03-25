10611bf745b7f437ccfe538f4bc70b4f6c09274f
twobit <twobit@lavabit.com>
2014-03-25 01:34:52 +0100

    loopy loop

--- index/messung/energy.py
+++ index/messung/energy.py
@@ -3,6 +3,10 @@
 import sys, os, time, socket
 
 PORT = 1337
+SAMPLES = 3
+freqs = [ 30, 48, 60, 72, 84, 96, 120, 132, 144, 156, 168, 180, 192, 204,
+	216, 240, 264, 288, 336, 360, 384, 408, 480, 528, 600, 648, 672, 696,
+	720, 744, 768, 816, 864, 912, 960, 1008 ]
 
 
 def server():
@@ -28,11 +32,13 @@ def server():
 			input_len = cmd["input_len"]
 
 			os.system("cpufreq-set --governor userspace")
-			os.system("cpufreq-set --min %d" % freq)
-			os.system("cpufreq-set --max %d" % freq)
-			os.system("cpufreq-set --freq %d" % freq)
+			os.system("cpufreq-set --min %d000" % freq)
+			os.system("cpufreq-set --max %d000" % freq)
+			os.system("cpufreq-set --freq %d000" % freq)
 			os.system("echo %d > /sys/devices/system/cpu/cpu1/online" % int(cpus == 2))
 			time.sleep(1)
+			f = int(file("/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq").read()) / 1000
+			assert freq == f, "%d != %d" % (freq, f)
 
 			conn.sendall("start") # tell client to begin tracking current
 			out = os.popen("../index %s ../wiki/test_%d.txt" % (mode, input_len)).read()
@@ -41,6 +47,10 @@ def server():
 			print " %4d | %7d | %11d | %11d | %9.3f" % (
 				cpus, 1 + ("t" in mode), freq, input_len, float(out))
 		conn.close()
+
+
+
+################################################################################
 
 
 
@@ -59,37 +69,47 @@ def client(host):
 	import serial
 	cambri = serial.Serial("/dev/ttyUSB0", 115200, 8, "N", 1)
 
-	while 1:
-		cpus = 2
-		mode = "tmr"
-		freq = 240
-		input_len = 50
+	print " cpus | threads | freq in MHz | input in MB | time in s | current in mA"
+	print "------+---------+-------------+-------------+-----------+---------------"
 
-		# send command
-		s.sendall(repr({"cpus":cpus, "mode":mode, "freq":freq, "input_len":input_len}))
-		start = s.recv(1024)
+	for cpus in [1, 2]:
+		modes = ["mr"]
+		if cpus == 2, modes.append("tmr")
+		for mode in modes:
+			for freq in freqs:
+				for input_len in 25, 50, 100:
+					time = 0
+					current = 0
+					for i in range(SAMPLES):
 
-		# track current
-		s.setblocking(0)
-		currents = []
-		while 1:
-			currents.append(read_current(cambri, 1))
-			try:
-				time = float(s.recv(1024))
-				break
-			except:
-				continue
-		s.setblocking(1)
+						# send command
+						s.sendall(repr({"cpus":cpus, "mode":mode, "freq":freq, "input_len":input_len}))
+						start = s.recv(1024)
 
-		# estimate mean
-		c = sum(currents) / float(len(currents))
-		print " cpus | threads | freq in MHz | input in MB | time in s | current in mA"
-		print "------+---------+-------------+-------------+-----------+---------------"
-		print " %4d | %7d | %11d | %11d | %9.3f | %13.2f" % (
-			cpus, 1 + ("t" in mode), freq, input_len, time, c)
+						# track current
+						s.setblocking(0)
+						currents = []
+						while 1:
+							currents.append(read_current(cambri, 1))
+							try:
+								t = float(s.recv(1024))
+								break
+							except:
+								continue
+						s.setblocking(1)
 
-		break
+						# estimate mean
+						c = sum(currents) / float(len(currents))
 
+
+
+						time += t
+						current += c
+					time /= SAMPLES
+					current /= SAMPLES
+
+					print " %4d | %7d | %11d | %11d | %9.3f | %13.2f" % (
+						cpus, 1 + ("t" in mode), freq, input_len, time, current)
 
 	s.close()
 
