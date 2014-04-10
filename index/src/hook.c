e11f23c72e5871db3e5d7657e7ca54390eb58bbe
twobit <twobit@lavabit.com>
2014-04-11 00:45:55 +0200

    arm compatibility for hook

--- index/src/hook.c
+++ index/src/hook.c
@@ -37,7 +37,7 @@ static void my_free_hook(void *ptr, const void *caller) {
 
 	pthread_spin_unlock(&malloc_spin);
 }
-
+PTHREAD_PROCESS_SHARED
 static void my_init_hook(void) {
 	pthread_spin_init(&malloc_spin, 0);
 
@@ -46,6 +46,10 @@ static void my_init_hook(void) {
 	__malloc_hook = my_malloc_hook;
 	__free_hook = my_free_hook;
 }
+
+#ifdef __arm__
+#define __MALLOC_HOOK_VOLATILE
+#endif
 
 void (*__MALLOC_HOOK_VOLATILE __malloc_initialize_hook)(void) = my_init_hook;
 
