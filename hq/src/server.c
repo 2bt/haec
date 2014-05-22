b45b9d5818c7af399e0772253bca2bdd54fd04da
twobit <twobit@lavabit.com>
2014-05-22 13:40:57 +0200

    server: handle multiple connetions synchronously

--- hq/src/server.c
+++ hq/src/server.c
@@ -9,6 +9,8 @@
 #include <racr/racr.h>
 
 
+enum { STDIN = 0 };
+
 
 Scheme_Object* eval_script(Scheme_Env* env, const char* filename) {
 	FILE* f = fopen(filename, "r");
@@ -29,12 +31,11 @@ int main(int argc, char** argv) {
 	eval_script(env, "scheme.scm");
 
 
-
 	// tcp server code
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) error(1, 0, "socket\n");
+	int listener = socket(AF_INET, SOCK_STREAM, 0);
+	if (listener == -1) error(1, 0, "socket\n");
 	int yes = 1;
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0) {
+	if (setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0) {
 		error(1, 0, "setsockopt");
 	}
 	struct sockaddr_in server = {
@@ -42,31 +43,72 @@ int main(int argc, char** argv) {
 		htons(1337),
 		{ INADDR_ANY }
 	};
-	if (bind(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0) {
+	if (bind(listener, (struct sockaddr*)&server, sizeof(server)) < 0) {
 		error(1, 0, "bind");
 	}
-	listen(sockfd, 3);
+	listen(listener, 3);
+
+	fd_set master;
+	FD_ZERO(&master);
+	FD_SET(STDIN, &master);
+	FD_SET(listener, &master);
+	int fdmax = listener;
+
 
 
 	for (;;) {
-		struct sockaddr_in client;
-		socklen_t size = sizeof(client);
-		int new_fd = accept(sockfd, (struct sockaddr *)&client, &size);
-		if (new_fd < 0) {
-			perror("accept");
-			continue;
+		fd_set readfds = master;
+		if (select(fdmax + 1, &readfds, NULL, NULL, NULL) < 0) {
+			error(1, 0, "select");
 		}
 
-		char s[INET_ADDRSTRLEN];
-		inet_ntop(client.sin_family, &client.sin_addr, s, sizeof(s));
-		printf("server: got connection from %s\n", s);
+		int i;
+		for (i = 0; i <= fdmax; i++) {
+			if (FD_ISSET(i, &readfds)) {
+				if (i == STDIN) {
+					char msg[256];
+					fgets(msg, sizeof(msg), stdin);
+					printf("stdin: %s", msg);
+				}
+				else if (i == listener) {
+					// handle new connection
+
+					struct sockaddr_in client;
+					socklen_t size = sizeof(client);
+					int newfd = accept(listener, (struct sockaddr *)&client, &size);
+					if (newfd < 0) {
+						perror("accept");
+					}
+					else {
+						FD_SET(newfd, &master);
+						if (newfd > fdmax) fdmax = newfd;
 
 
-		if (send(new_fd, "Hello, world!", 13, 0) < 0) {
-			perror("send");
+						char s[INET_ADDRSTRLEN];
+						inet_ntop(client.sin_family, &client.sin_addr, s, sizeof(s));
+						printf("new connection from %s on socket %d\n", s, newfd);
+					}
+				}
+				else {
+					// recv from client
+					char msg[1024];
+					int len = recv(i, msg, sizeof(msg), 0);
+					if (len <= 0) {
+						close(i);
+						FD_CLR(i, &master);
+						printf("socket %d hung up\n", i);
+					}
+					else {
+						printf("received %d bytes from socket %d: %s\n", len, i, msg);
+
+
+					}
+				}
+			}
 		}
-		close(new_fd);
+
 	}
+
 
 	return 0;
 }
