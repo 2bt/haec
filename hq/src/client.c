d5315204e863a130bc27c0a568cd1c55fb36a79e
twobit <twobit@lavabit.com>
2014-05-22 12:17:13 +0200

    rewrite network code

--- hq/src/client.c
+++ hq/src/client.c
@@ -9,28 +9,30 @@
 
 int main(int argc, char** argv) {
 
-	struct addrinfo hints = { 0, AF_INET, SOCK_STREAM };
-	struct addrinfo* servinfo;
-	if (getaddrinfo("localhost", "1337", &hints, &servinfo) != 0) {
-		error(1, 0, "getaddrinfo");
-	}
-	int sockfd = socket(servinfo->ai_family,
-						servinfo->ai_socktype,
-						servinfo->ai_protocol);
-	if (sockfd == -1) error(1, 0, "socket\n");
+
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) error(1, 0, "socket\n");
+	struct sockaddr_in server = {
+		AF_INET,
+		htons(1337),
+		{ inet_addr("127.0.0.1") }
+	};
 
 
-	if (connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+	if (connect(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0) {
 		close(sockfd);
-		perror("client: connect");
+		error(1, 0, "connect");
 	}
 
 
 	char s[INET_ADDRSTRLEN];
-	struct sockaddr_in* addr_in = (struct sockaddr_in*) servinfo->ai_addr;
-	inet_ntop(servinfo->ai_family, &addr_in->sin_addr, s, sizeof(s));
+	inet_ntop(AF_INET, &server.sin_addr, s, sizeof(s));
 	printf("client: connecting to %s\n", s);
-	freeaddrinfo(servinfo);
+
+	char msg[256];
+	ssize_t l = recv(sockfd, msg, sizeof(msg), 0);
+	fwrite(msg, 1, l, stdout);
+	puts("");
 
 	close(sockfd);
 
--- hq/src/server.c
+++ hq/src/server.c
@@ -23,60 +23,50 @@ Scheme_Object* eval_script(Scheme_Env* env, const char* filename) {
 }
 
 
-
 int main(int argc, char** argv) {
 
 	RACR_INIT(env, "bytecode", NULL);
 	eval_script(env, "scheme.scm");
 
 
+
 	// tcp server code
-
-	struct addrinfo hints = { AI_PASSIVE, AF_INET, SOCK_STREAM };
-	struct addrinfo* servinfo;
-	if (getaddrinfo(NULL, "1337", &hints, &servinfo) != 0) {
-		error(1, 0, "getaddrinfo");
-	}
-
-	int sockfd = socket(servinfo->ai_family,
-						servinfo->ai_socktype,
-						servinfo->ai_protocol);
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) error(1, 0, "socket\n");
-
 	int yes = 1;
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0) {
 		error(1, 0, "setsockopt");
 	}
-	if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+	struct sockaddr_in server = {
+		AF_INET,
+		htons(1337),
+		{ INADDR_ANY }
+	};
+	if (bind(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0) {
 		error(1, 0, "bind");
 	}
+	listen(sockfd, 3);
 
-	freeaddrinfo(servinfo);
-
-	listen(sockfd, 10);
 
 	for (;;) {
-		struct sockaddr_in their_addr;
-		socklen_t sin_size = sizeof(their_addr);
-		int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-		if (new_fd == -1) {
+		struct sockaddr_in client;
+		socklen_t size = sizeof(client);
+		int new_fd = accept(sockfd, (struct sockaddr *)&client, &size);
+		if (new_fd < 0) {
 			perror("accept");
 			continue;
 		}
 
-
 		char s[INET_ADDRSTRLEN];
-		inet_ntop(their_addr.sin_family, &their_addr.sin_addr, s, sizeof(s));
+		inet_ntop(client.sin_family, &client.sin_addr, s, sizeof(s));
 		printf("server: got connection from %s\n", s);
 
 
-
-		if (send(new_fd, "Hello, world!", 13, 0) == -1) {
+		if (send(new_fd, "Hello, world!", 13, 0) < 0) {
 			perror("send");
 		}
 		close(new_fd);
 	}
-
 
 	return 0;
 }
