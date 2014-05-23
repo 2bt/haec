d2beff10a6f4721754f9b4c4cf562b14ec1f757f
twobit <twobit@lavabit.com>
2014-05-23 21:32:24 +0200

    begin ast spec draft

--- communication/scheme.scm
+++ communication/scheme.scm
@@ -1,1 +1,14 @@
-(display "hi\n")
+(define hq-spec (create-specification))
+
+(with-specification hq-spec
+
+ (ast-rule 'Configurator->Configuration*-Profile*)
+ (ast-rule 'Configuration->mode-memory)
+ (ast-rule 'Profile->modes)
+
+ (compile-ast-specifications 'Configurator)
+
+ (compile-ag-specifications))
+
+
+(display "done.\n")
