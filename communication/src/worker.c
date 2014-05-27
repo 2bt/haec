22af19f32788facf61a1ce782b15d017d4879f38
twobit <twobit@lavabit.com>
2014-05-27 12:23:30 +0200

    finish worker command implementation

--- communication/src/worker.c
+++ communication/src/worker.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <error.h>
 
@@ -14,43 +15,47 @@ enum { PORT = 1337 };
 int sockfd;
 
 
-void* worker_thread(void* data) {
-	printf("worker hi\n");
+void* worker_thread(char* arg) {
+	if (arg) { // parse arg string
+		char* p = strchr(arg, ' ');
+		if (p) *p++ = '\0';
+	}
 
 	char line[1024];
-	snprintf(line, sizeof(line), "../index/index -mr 0 ../index/index/wiki/test_25.txt");
-	FILE* p = popen(line, "r");
-	size_t len = fread(line, 1, sizeof(line), p);
-	if (len <= 0) {
-		printf("worker error\n");
-	}
-	else {
-		printf("index output: %s", line);
-	}
+	snprintf(line, sizeof(line), "../index/index mr 0 ../index/wiki/test_25.txt");
+	FILE* f = popen(line, "r");
+	size_t len = fread(line, 1, sizeof(line), f);
+	if (len <= 0) printf("worker %s error\n", arg);
+	else printf("worker %s output: %.*s", arg, len, line);
+	pclose(f);
 
-	pclose(p);
+	snprintf(line, sizeof(line), "worker %s done", arg);
+	printf("%s\n", line);
+	send(sockfd, line, strlen(line) + 1, 0);
 
-	printf("worker bye\n");
-	send(sockfd, "worker bye", 10, 0);
-
+	free(arg);
 	return NULL;
 }
 
 
 void handle_command(char* cmd) {
 	char* p = strchr(cmd, ' ');
-	if (p) *p++ = '\0';
-	if (strcmp(cmd, "index") == 0) {
-		printf("command: index [%s]\n", p);
+	if (!p) {
+		printf("error parsing command: %s\n", cmd);
+		return;
+	}
+	*p++ = '\0';
+	printf("command: %s %s\n", cmd, p);
+
+	if (strcmp(cmd, "worker") == 0) {
 		pthread_t worker;
-		pthread_create(&worker, NULL, worker_thread, NULL);
-
+		pthread_create(&worker, NULL, worker_thread, strdup(p));
+	}
+	if (strcmp(cmd, "cpu") == 0) {
 
 
 	}
-	else {
-		printf("unknown command: %s [%s]\n", cmd, p);
-	}
+	else printf("unknown command\n");
 }
 
 
