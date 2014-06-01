31e9330fbf1292c32f6184a881b3c5a6ad3b8de3
twobit <twobit@lavabit.com>
2014-06-01 21:42:12 +0200

    save dump file series of increasing size

--- index/wiki/unpack.py
+++ index/wiki/unpack.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 from lxml import etree
 import bz2
+import shutil
 
 """
 This script lazily decompresses a wikipedia dump file.
 Articles are written out, each followed by a NULL-byte indicating its end.
-The script terminates when the output file size exceeds min_size.
 
 The latest dump file can be found here:
 http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
 """
 
 
-path = "enwiki-latest-pages-articles.xml.bz2"
-fout = file("test.txt", "w")
-min_size = 2 * 2**30 # 2GB
 
+path = "enwiki-latest-pages-articles.xml.bz2"
+fout = file("dump.txt", "w")
+articles = 0
+min_size = size_step = 2**24 # 16 MB step size
 
 text_tag = "{http://www.mediawiki.org/xml/export-0.8/}text"
 title_tag = "{http://www.mediawiki.org/xml/export-0.8/}title"
-articles = 0
 for _, e in etree.iterparse(bz2.BZ2File(path)):
 #	if e.tag == title_tag: print e.text.encode("u8")
 	if e.tag == text_tag and not e.text.startswith("#REDIRECT"):
@@ -29,5 +29,11 @@ for _, e in etree.iterparse(bz2.BZ2File(path)):
 			print "size:", fout.tell()
 			print "articles:", articles
 			fout.close()
-			break
+			shutil.copyfile("dump.txt", "dump_%04d.txt" % (min_size >> 20))
+			fout = file("dump.txt", "a")
+			min_size += size_step
 	e.clear()
+
+
+
+
