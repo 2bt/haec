2d6f8056cd619df9bfd411db862a9bd74f0c84b7
twobit <twobit@lavabit.com>
2014-06-03 00:54:06 +0200

    refactor work args

--- communication/src/worker.c
+++ communication/src/worker.c
@@ -14,48 +14,57 @@ enum { PORT = 1337 };
 
 int sockfd;
 
+typedef struct {
+	int id;
+	int threads;
+	int size;
+} WorkArgs;
 
-void* worker_thread(char* arg) {
-	if (arg) { // parse arg string
-		char* p = strchr(arg, ' ');
-		if (p) *p++ = '\0';
-	}
 
+void* work_thread(WorkArgs* args) {
 	char line[1024];
-	snprintf(line, sizeof(line), "../index/index mr 0 ../index/wiki/test_25.txt");
+	snprintf(line, sizeof(line),
+		"../index/index mr %d ../index/wiki/dump_%04d.txt",
+		args->threads, args->size);
+
 	FILE* f = popen(line, "r");
 	size_t len = fread(line, 1, sizeof(line), f);
-	if (len <= 0) printf("worker %s error\n", arg);
-	else printf("worker %s output: %.*s", arg, len, line);
+	if (len <= 0) printf("work %d error\n", args->id);
+	else printf("work %d output: %.*s", args->id, len, line);
 	pclose(f);
 
-	snprintf(line, sizeof(line), "worker %s done", arg);
+	snprintf(line, sizeof(line), "work %d done", args->id);
 	printf("%s\n", line);
 	send(sockfd, line, strlen(line) + 1, 0);
 
-	free(arg);
+	free(args);
 	return NULL;
 }
 
 
 void handle_command(char* cmd) {
 	char* p = strchr(cmd, ' ');
-	if (!p) {
-		printf("error parsing command: %s\n", cmd);
-		return;
-	}
+	if (!p) goto ERROR;
 	*p++ = '\0';
 	printf("command: %s %s\n", cmd, p);
 
-	if (strcmp(cmd, "worker") == 0) {
-		pthread_t worker;
-		pthread_create(&worker, NULL, worker_thread, strdup(p));
+	if (strcmp(cmd, "work") == 0) {
+		WorkArgs* args = malloc(sizeof(WorkArgs));
+		if (sscanf(p, "%d %d %d",
+			&args->id, &args->threads, &args->size) != 3) goto ERROR;
+		pthread_t work;
+		pthread_create(&work, NULL, work_thread, args);
 	}
-	if (strcmp(cmd, "cpu") == 0) {
-
+	else if (strcmp(cmd, "cpu") == 0) {
+		int cpus, freq;
+		if (sscanf(p, "%d %d", &cpus, &freq) != 2) goto ERROR;
+		// TODO...
 
 	}
 	else printf("unknown command\n");
+	return;
+ERROR:
+	printf("error parsing command: %s\n", cmd);
 }
 
 
@@ -67,7 +76,6 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 
-
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) error(1, 0, "socket\n");
 	struct sockaddr_in server = { AF_INET, htons(PORT),
@@ -77,7 +85,6 @@ int main(int argc, char** argv) {
 		close(sockfd);
 		error(1, 0, "connect");
 	}
-
 
 	for (;;) {
 		char msg[1024];
