2485f1ac042cc21b1da8b4583ad7ea6fcb684752
twobit <twobit@lavabit.com>
2014-06-17 09:14:23 +0200

    move command code into its own function

--- communication/src/server.c
+++ communication/src/server.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <error.h>
 #include <arpa/inet.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "server.h"
@@ -10,13 +11,18 @@
 
 enum { PORT = 1337 };
 
+// time in seconds
+const double TIME_BOOTING = 20.0; // STATE_BOOTING -> STATE_IDLE
+const double TIME_HALTING = 5.0; // STATE_HALTING -> STATE_OFF
+
 
 enum {
 	STATE_OFF,
 	STATE_BOOTING,
 	STATE_IDLE,
 	STATE_WORKING,
-	STATE_HALTING
+	STATE_HALTING,
+	STATE_ERROR,
 };
 
 const char* state_strings[] = {
@@ -24,7 +30,8 @@ const char* state_strings[] = {
 	"BOOTING",
 	"IDLE",
 	"WORKING",
-	"HALTING"
+	"HALTING",
+	"ERROR",
 };
 
 
@@ -36,7 +43,7 @@ typedef struct {
 	int socket_fd;
 
 	int state;
-	// TODO: timestamp of last state change
+	double timestamp;	// timestamp of the last state change
 
 /*
 	// config
@@ -58,12 +65,19 @@ enum { WORKER_COUNT = 3 };
 Worker workers[WORKER_COUNT];
 
 
+double timestamp(void) {
+	struct timespec t;
+	clock_gettime(CLOCK_REALTIME, &t);
+	return t.tv_sec + t.tv_nsec * 1e-9;
+}
+
 static void init_workers(void) {
 	static const char* a[WORKER_COUNT] = {
-		"127.0.0.1",
+		"192.168.1.42",
 		"127.0.0.1",
 		"127.0.0.1"
 	};
+	double time = timestamp();
 	int i;
 	for (i = 0; i < WORKER_COUNT; i++) {
 		Worker* w = &workers[i];
@@ -72,6 +86,7 @@ static void init_workers(void) {
 		w->port = 0;
 		w->socket_fd = -1;
 		w->state = STATE_OFF;
+		w->timestamp = time;
 	}
 }
 
@@ -96,17 +111,64 @@ Worker* get_worker_by_socket(int s) {
 }
 
 
+Worker* get_worker_by_cambri_port(int i) {
+	if (i < 1 || i > WORKER_COUNT) return NULL;
+	return &workers[i - 1];
+}
 
 
 
 
-enum { STDIN = 0 };
 
 struct {
 	fd_set fds;
 	int fdmax;
-
+	int running;
 } server;
+
+static void server_command() {
+
+	char msg[1024];
+	fgets(msg, sizeof(msg), stdin);
+	msg[strlen(msg) - 1] = '\0';
+	printf("stdin: %s\n", msg);
+
+
+	if (strcmp(msg, "exit") == 0) {
+		server.running = 0;
+		printf("exiting...\n");
+	}
+	if (strcmp(msg, "status") == 0) {
+		printf(" cambri port | address:port          | socket | state   | since \n");
+		printf("-------------+-----------------------+--------+---------+-------\n");
+		int i;
+		for (i = 0; i < WORKER_COUNT; i++) {
+			double time = timestamp();
+			Worker* w = &workers[i];
+			char s[INET_ADDRSTRLEN];
+			inet_ntop(AF_INET, &w->addr, s, sizeof(s));
+			printf("%12d | %-15s:%05d | %6d | %-7s | %5.2f\n",
+				w->cambri_port, s, w->port, w->socket_fd,
+				state_strings[w->state], time - w->timestamp);
+		}
+	}
+	else {
+		// TESTING
+		int p = atoi(msg);
+		char* s = strchr(msg, ' ');
+		if (!s) {
+			printf("command error\n");
+			return;
+		}
+		s++;
+		Worker* w = get_worker_by_cambri_port(p);
+		if (!w) {
+			printf("command error\n");
+			return;
+		}
+		send(w->socket_fd, s, strlen(s) + 1, 0);
+	}
+}
 
 
 static void server_new_connection(int s) {
@@ -131,24 +193,27 @@ static void server_new_connection(int s) {
 			w->port = client.sin_port;
 			w->socket_fd = newfd;
 			w->state = STATE_IDLE;
+			w->timestamp = timestamp();
 
 			char s[INET_ADDRSTRLEN];
 			inet_ntop(AF_INET, &w->addr, s, sizeof(s));
-			printf("new connection from %s:%d on socket %d\n", s, w->port, w->socket_fd);
+			printf("worker %d connected from %s:%d on socket %d\n", w->cambri_port, s, w->port, w->socket_fd);
 		}
 	}
 }
 
+
 static void server_receive(int s) {
 	Worker* w = get_worker_by_socket(s);
+	if (!w) printf("unknown receiver socker: %d\n", s);
 
 	char msg[1024];
 	size_t len = recv(s, msg, sizeof(msg), 0);
 	if (len <= 0) {
 		close(s);
 		FD_CLR(s, &server.fds);
-		printf("socket %d hung up\n", s);
 		if (w) {
+			printf("worker %d hung up\n", w->cambri_port);
 			w->socket_fd = -1;
 			w->port = 0;
 			w->state = STATE_HALTING;
@@ -156,10 +221,13 @@ static void server_receive(int s) {
 		return;
 	}
 
-
-	printf("received %d bytes from socket %d: %.*s\n", (int) len, s, (int) len, msg);
+	if (w) {
+		printf("received %d bytes from worker %d: %.*s\n", (int) len, w->cambri_port, (int) len, msg);
+	}
 }
 
+
+enum { STDIN = 0 };
 
 void server_run(void) {
 
@@ -179,13 +247,13 @@ void server_run(void) {
 	FD_SET(STDIN, &server.fds);
 	FD_SET(listener, &server.fds);
 	server.fdmax = listener;
+	server.running = 1;
 
 
 	init_workers();
 
-	printf("entering event loop.\n");
-	int running = 1;
-	while (running) {
+	printf("entering server loop.\n");
+	while (server.running) {
 		fd_set fds = server.fds;
 		struct timeval timeout = { 0, 100000 };
 		int count = select(server.fdmax + 1, &fds, NULL, NULL, &timeout);
@@ -195,39 +263,22 @@ void server_run(void) {
 		int i;
 		for (i = 0; i <= server.fdmax; i++) {
 			if (FD_ISSET(i, &fds)) {
-
-
-				if (i == STDIN) {
-					// command from stdin
-
-					char msg[1024];
-					fgets(msg, sizeof(msg), stdin);
-					msg[strlen(msg) - 1] = '\0';
-					printf("stdin: %s\n", msg);
-
-
-					if (strcmp(msg, "exit") == 0) {
-						running = 0;
-						printf("exiting...\n");
-					}
-					if (strcmp(msg, "status") == 0) {
-						int i;
-						for (i = 0; i < WORKER_COUNT; i++) {
-							Worker* w = &workers[i];
-							char s[INET_ADDRSTRLEN];
-							inet_ntop(AF_INET, &w->addr, s, sizeof(s));
-							printf("%d %s:%d %d %s\n",
-								w->cambri_port, s, w->port, w->socket_fd, state_strings[w->state]);
-						}
-					}
-					else {
-						// TESTING
-						send(server.fdmax, msg, strlen(msg) + 1, 0);
-					}
-
-				}
+				if (i == STDIN) server_command();
 				else if (i == listener) server_new_connection(listener);
 				else server_receive(i);
+			}
+		}
+
+		double time = timestamp();
+		for (i = 0; i < WORKER_COUNT; i++) {
+			Worker* w = &workers[i];
+
+			if (w->state == STATE_HALTING
+			&& time - w->timestamp > TIME_HALTING) {
+				w->state = STATE_OFF;
+				// TODO: cambri power shutdown
+
+
 			}
 		}
 	}
