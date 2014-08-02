8e246350b5cab628b8c1928269129593a0f7990a
twobit <twobit@lavabit.com>
2014-08-02 13:31:00 +0200

    clean protocol

--- communication/src/worker_main.c
+++ communication/src/worker_main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
 #include <error.h>
 
@@ -13,16 +14,20 @@
 
 enum { PORT = 1337 };
 
+ssize_t sendf(int s, const char* format, ...) {
+	char line[256];
+	va_list args;
+	va_start(args, format);
+	vsnprintf(line, sizeof(line), format, args);
+	va_end(args);
+	printf("send: %s\n", line);
+	return send(s, line, strlen(line) + 1, 0);
+}
 
-int sockfd;
+int socket_fd;
 
 
-typedef struct {
-	int id;
-	int threads;
-	int input_len;
-} WorkArgs;
-
+typedef struct { int id; int threads; int input_len; } WorkArgs;
 
 void* work_thread(WorkArgs* args) {
 	char line[256];
@@ -32,24 +37,14 @@ void* work_thread(WorkArgs* args) {
 
 	FILE* f = popen(line, "r");
 	size_t len = fread(line, 1, sizeof(line), f);
-	if (len <= 0) printf("work %d error\n", args->id);
+	if (len <= 0) printf("work-error %d\n", args->id);
 	else printf("work %d output: %.*s", args->id, (int) len, line);
 	int ret = pclose(f);
 
-	snprintf(line, sizeof(line), "work %d done %d", args->id, ret);
-	printf("%s\n", line);
-	send(sockfd, line, strlen(line) + 1, 0);
+	sendf(socket_fd, "work-complete %d %d", args->id, ret);
 
 	free(args);
 	return NULL;
-}
-
-
-
-void send_ack(int e) {
-	char line[64];
-	snprintf(line, sizeof(line), "ack %d", e);
-	send(sockfd, line, strlen(line) + 1, 0);
 }
 
 
@@ -68,8 +63,7 @@ void handle_command(char* cmd) {
 			&args->id, &args->threads, &args->input_len) != 3) goto ERROR;
 		pthread_t work;
 		int e = pthread_create(&work, NULL, (void*(*)(void*)) work_thread, args);
-		if (e != 0) printf("work error %d\n", e);
-		send_ack(e);
+		sendf(socket_fd, "work-ack %d", e);
 	}
 	else if (strcmp(cmd, "cpu") == 0) {
 
@@ -81,8 +75,7 @@ void handle_command(char* cmd) {
 		if (cpus != 1 && cpus != 2) goto ERROR;
 		FILE* f = fopen("/sys/devices/system/cpu/cpu1/online", "w");
 		if (!f) {
-			printf("cpu error 1\n");
-			send_ack(1);
+			sendf(socket_fd, "cpu-ack 1");
 			return;
 		}
 		fprintf(f, cpus == 2 ? "1\n" : "0\n");
@@ -96,37 +89,32 @@ void handle_command(char* cmd) {
 			"cpufreq-set --max 1008000 && "
 			"cpufreq-set --freq %d000", freq);
 		if (system(line) != 0) {
-			printf("cpu error 2\n");
-			send_ack(2);
+			sendf(socket_fd, "cpu-ack 2");
 			return;
 		}
-		send_ack(0);
+		sendf(socket_fd, "cpu-ack 0");
 
 	}
 	else if (strcmp(cmd, "mem") == 0) {
 		FILE* f = popen("/usr/bin/free", "r");
 		if (!f) {
-			printf("mem error\n");
-			send_ack(-1);
+			sendf(socket_fd, "mem-ack -1");
 			return;
 		}
 		int mem = -1;
 		fscanf(f, "%*s %*s %*s %*s %*s %*s %*s %*d %*d %d", &mem);
 		fclose(f);
-		send_ack(mem);
+		sendf(socket_fd, "mem-ack %d", mem);
 	}
 	else if (strcmp(cmd, "halt") == 0) {
-
 		int e = system("halt");
-		if (e != 0) printf("halt error %d\n", e);
-		send_ack(e);
+		sendf(socket_fd, "halt-ack %d", e);
 	}
 
 	else goto ERROR;
 	return;
 ERROR:
-	printf("error parsing command: %s\n", cmd);
-	send_ack(1337);
+	sendf(socket_fd, "%s-ack 1337", cmd);
 }
 
 
@@ -138,22 +126,22 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) error(1, 0, "socket\n");
+	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (socket_fd < 0) error(1, 0, "socket\n");
 	struct sockaddr_in server = { AF_INET, htons(PORT),
 		{ inet_addr(argc == 2 ? argv[1] : "127.0.0.1" ) }
 	};
-	if (connect(sockfd, (struct sockaddr*)&server, sizeof(server)) < 0) {
-		close(sockfd);
+	if (connect(socket_fd, (struct sockaddr*)&server, sizeof(server)) < 0) {
+		close(socket_fd);
 		error(1, 0, "connect");
 	}
 
 	for (;;) {
 		char msg[1024];
-		ssize_t len = recv(sockfd, msg, sizeof(msg), 0);
+		ssize_t len = recv(socket_fd, msg, sizeof(msg), 0);
 		if (len <= 0) {
 			printf("server hung up\n");
-			close(sockfd);
+			close(socket_fd);
 			break;
 		}
 		// parse commands
