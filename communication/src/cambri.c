6dc8ea48146b2f5f9c825c024296dfe941642e17
twobit <twobit@lavabit.com>
2014-09-08 23:33:58 +0200

    check for cambri_enabled

--- communication/src/cambri.c
+++ communication/src/cambri.c
@@ -16,6 +16,9 @@ static int cambri_fd;
 static FILE* cambri_log_file;
 
 
+static int cambri_enabled = 0;
+
+
 void cambri_init(void) {
 	cambri_fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY);
 	if (cambri_fd < 0) error(1, 0, "cambri_init");
@@ -34,6 +37,7 @@ void cambri_init(void) {
 
 
 	cambri_log_file = fopen("cambri.log", "w");
+	if (!cambri_log_file) error(1, 0, "cambri_init");
 	fprintf(cambri_log_file, " time      ");
 	int i;
 	for (i = 1; i <= 8; i++) fprintf(cambri_log_file, " | %4d", 1000 + i);
@@ -42,10 +46,13 @@ void cambri_init(void) {
 	for (i = 1; i <= 8; i++) fprintf(cambri_log_file, "-+-----");
 	fprintf(cambri_log_file, "\n");
 	fflush(cambri_log_file);
+
+	cambri_enabled = 1;
 }
 
 
 void cambri_kill(void) {
+	if (!cambri_enabled) return;
 	close(cambri_fd);
 	fclose(cambri_log_file);
 }
@@ -74,6 +81,7 @@ static int cambri_read(char* buf, int len) {
 
 
 void cambri_log_current(const char* time) {
+	if (!cambri_enabled) return;
 	cambri_write("state");
 	char buf[1024] = {};
 	int ret = cambri_read(buf, sizeof(buf));
@@ -94,6 +102,7 @@ void cambri_log_current(const char* time) {
 
 
 void cambri_set_mode(int id, int mode) {
+	if (!cambri_enabled) return;
 	cambri_write("mode %c %d 4", mode, id % 10);
 	char buf[1024] = {};
 	cambri_read(buf, sizeof(buf));
