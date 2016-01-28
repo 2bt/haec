909125c6a0909324b08c39ba28f1263855a97e9a
twobit <s8572327@gmail.com>
2016-01-28 15:11:34 +0100

    gen config better

--- communication/gen_config.py
+++ communication/gen_config.py
@@ -5,9 +5,10 @@ import sys
 
 depth = int(sys.argv[1]) if len(sys.argv) > 1 else 2
 
-workers = []
-switches = []
 
+
+lines = []
+switches_left = []
 
 counter = 0
 def get_new_id():
@@ -16,19 +17,24 @@ def get_new_id():
 	counter += 1
 	return i
 
-
-def switch(depth, parent_id=0):
+def new_switch(parent_id, depth):
 	if depth == 0: return
-	my_id = get_new_id()
-	switches.append("switch %d %d" % (my_id, parent_id))
-
-	for _ in range(5): workers.append("worker %d %d 0.0.0.0 CUBIEBOARD" % (get_new_id(), my_id))
-	for _ in range(2): switch(depth - 1, my_id)
+	new_id = get_new_id()
+	lines.append("switch %d %d" % (new_id, parent_id))
+	switches_left.append((new_id, depth))
 
 
+def expand_switch(parent_id, depth):
+	for _ in range(5):
+		lines.append("worker %d %d 0.0.0.0 CUBIEBOARD" % (get_new_id(), parent_id))
+	for _ in range(2):
+		new_switch(parent_id, depth - 1)
 
-switch(depth)
+new_switch(0, depth)
 
-print "\n".join(switches)
-print
-print "\n".join(workers)
+while switches_left:
+	parent_id, depth = switches_left.pop(0)
+	expand_switch(parent_id, depth)
+
+
+print "\n".join(lines)
