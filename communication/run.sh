c337d8b9531c2b4662260cb7645622fcf70a0cc6
twobit <s8572327@gmail.com>
2016-04-26 17:45:10 +0200

    foo

--- communication/run.sh
+++ communication/run.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
 
-if [ ! $1 ]
-then
-	echo "usage:"
-	echo "$0 batman"
-	echo "$0 robin"
-	echo "$0 allin"
-	exit
-fi
 
+#cat pre_"$1".run ../rungengui/test6_90.run > q.run && ./server q.run; ../plot_run
 
-cat pre_"$1".run ../rungengui/test6.run > q.run && ./server q.run; ../plot_run
+echo '# batman' >> q
+
+for x in `seq 10 10 100`
+do
+	echo "# $x %" >> q
+	cat pre_batman.run ../rungengui/test6_"$x".run > q.run
+	./server q.run | awk '/racr-time/{print $2}' >> q
+	./server q.run | awk '/racr-time/{print $2}' >> q
+	./server q.run | awk '/racr-time/{print $2}' >> q
+	./server q.run | awk '/racr-time/{print $2}' >> q
+	./server q.run | awk '/racr-time/{print $2}' >> q
+done
+
+echo '# robin' >> q
+
+for x in `seq 10 10 100`
+do
+	echo "# $x %" >> q
+	cat pre_robin.run ../rungengui/test6_"$x".run > q.run
+	./server q.run | awk '/racr-time/{print $2}' >> q
+	./server q.run | awk '/racr-time/{print $2}' >> q
+	./server q.run | awk '/racr-time/{print $2}' >> q
+	./server q.run | awk '/racr-time/{print $2}' >> q
+	./server q.run | awk '/racr-time/{print $2}' >> q
+done
